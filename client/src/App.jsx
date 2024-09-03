ee3fec431be87500b370da04ffd9197eebf62514
Bibek Ray <bi.ray1225@gmail.com>
2024-09-03 15:48:07 +0545

    chore : created store using redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello Bibek!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello Bibek!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
