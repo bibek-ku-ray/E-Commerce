3fffb8433af85dfb8c9ef761aaedfdaaac703977
Bibek Ray <bi.ray1225@gmail.com>
2024-09-03 15:15:33 +0545

    chore: Add server and client configuration files and directories

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mongo atlas
+PW - aq5ww3Uuq55SQO85
+US - devbibek25
+url - mongodb+srv://devbibek25:aq5ww3Uuq55SQO85@clusterecommece.dpyc8.mongodb.net/
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT | 3001;
+
+mongoose
+  .connect(
+    "mongodb+srv://devbibek25:aq5ww3Uuq55SQO85@clusterecommece.dpyc8.mongodb.net/"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Not connected to MongoDB :: ", err));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Exires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
