a335b33c4f86e625d79d58e32052e73ce25fffa1
Bibek Ray <bi.ray1225@gmail.com>
2024-09-08 14:56:08 +0545

    chore: Added layout of admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminProducts from "./pages/admin-view/products";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello Bibek!</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout/>}>
+          <Route path="login" element={<AuthLogin/>}/>
+          <Route path="register" element={<AuthRegister/>}/>
+        </Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="dashboard" element={<AdminDashboard/>}/>
+          <Route path="features" element={<AdminFeatures/>}/>
+          <Route path="orders" element={<AdminOrders/>}/>
+          <Route path="products" element={<AdminProducts/>}/>          
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
