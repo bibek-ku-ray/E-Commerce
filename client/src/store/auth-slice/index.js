7962a178cd5cacf3c9e2a44a5f18b2640969a5a6
Bibek Ray <bi.ray1225@gmail.com>
2024-09-10 22:39:47 +0545

    refactor: Update authentication handling in App component and add async thunk for user registration

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isAuthenticated: false,
@@ -6,11 +7,41 @@ const initialState = {
   user: null,
 };
 
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:3001/api/auth/register",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setUser: (state, action) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.user = null),
+          (state.isAuthenticated = false);
+      })
+      .addCase(registerUser.rejected, (state) => {
+        (state.isAuthenticated = false),
+          (state.isLoading = false),
+          (state.user = null);
+      });
   },
 });
 
