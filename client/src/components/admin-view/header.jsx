bfeb728f9467c7b55e1b68402892f00bfc97b786
Bibek Ray <bi.ray1225@gmail.com>
2024-10-09 14:36:28 +0545

    Refactor: Update AdminHeader and AdminSidebar components
    - Add logout functionality to AdminHeader component
    - Integrate Lucide icons and update styling in AdminSidebar component
    - Add new Sheet component for modal-like behavior
    
    Refactor the AdminHeader component to include a logout button that dispatches the loginUser action from the auth-slice. Update the AdminSidebar component to use Lucide icons for menu items and improve the styling. Additionally, introduce a new Sheet component for creating modal-like behavior in the UI.

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { AlignCenter, LogOut } from 'lucide-react'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '@/store/auth-slice'
 
-const AdminHeader = () => {
+const AdminHeader = ({setOpen}) => {
+  const dispatch = useDispatch()
+
+  function handleLogout(){
+    dispatch(loginUser())
+  }
   return (
-    <div>AdminHeader</div>
-  )
+      <header className="flex items-center justify-between px-4 py-3 border-b bg-background">
+          <Button 
+            onClick={() => setOpen(true)} 
+            className="lg:hidden sm:block"
+          >
+              <AlignCenter />
+              <span className="sr-only">Toggle Menu</span>
+          </Button>
+          <div className="flex justify-end flex-1">
+              <Button
+                  className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium rounded-md shadow"
+                  onClick={handleLogout}
+              >
+                  <LogOut />
+                  Logout
+              </Button>
+          </div>
+      </header>
+  );
 }
 
 export default AdminHeader
--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./sidebar";
 import AdminHeader from "./header";
 
 const AdminLayout = () => {
+   const [openSidebar, setOpenSidebar] = useState(false);
   return (
-    <div className="flex min-h-screen w-full">
-      <AdminSidebar />
-      <div className="flex flex-1 flex-col">
-        <AdminHeader />
-        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
-          <Outlet />
-        </main>
+      <div className="flex w-full min-h-screen">
+          <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
+          <div className="flex flex-col flex-1">
+              <AdminHeader setOpen={setOpenSidebar} />
+              <main className="flex flex-1 p-4 bg-muted/40 md:p-6">
+                  <Outlet />
+              </main>
+          </div>
       </div>
-    </div>
   );
 };
 
