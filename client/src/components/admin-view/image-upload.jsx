20d4dd5b475dcc1a1ee62f1df11cbcae48fad4a3
Bibek Ray <bi.ray1225@gmail.com>
2024-10-12 22:40:39 +0545

    Refactor form submit button position

---
+++ client/src/components/admin-view/image-upload.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductImageUpload = () => {
+  return (
+    <div className='w-full max-w-md mx-auto'>
+
+    </div>
+  )
+}
+
+export default ProductImageUpload
--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import React, { useState } from "react";
+
+const initialFormData = {
+    image: null,
+    title: "",
+    description: "",
+    category: "",
+    brand: "",
+    price: "",
+    salePrice: "",
+    totalStock: "",
+    averageReview: 0,
+};
+
+import ProductImageUpload from "@/components/admin-view/image-upload";
 
 const AdminProducts = () => {
-  return (
-    <div>AdminProducts</div>
-  )
-}
+    const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+        useState(false);
 
-export default AdminProducts
+    const [formData, setFormData] = useState(initialFormData)
+
+    function onSubmit(){}
+    return (
+        <>
+            <div className="flex justify-end w-full mb-5">
+                <Button onClick={() => setOpenCreateProductsDialog(true)}>
+                    Add New Product
+                </Button>
+            </div>
+            <div className="gap-4 gird md:grid-cols-3 lg:grid-cols-4"></div>
+            <Sheet
+                open={openCreateProductsDialog}
+                onOpenChange={() => {
+                    setOpenCreateProductsDialog(false);
+                }}
+            >
+                <SheetContent side="right" className="overflow-auto">
+                    <SheetHeader>
+                        <SheetTitle>Add New Sheet</SheetTitle>
+                    </SheetHeader>
+                    <ProductImageUpload />
+                    <div>
+                        <CommonForm
+                            onSubmit={onSubmit}
+                            formData={formData}
+                            setFormData={setFormData}
+                            buttonText={"Add"}
+                            formControls={addProductFormElements}
+                        />
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </>
+    );
+};
+
+export default AdminProducts;
